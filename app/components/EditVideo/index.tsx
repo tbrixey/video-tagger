e25e55eb03b3c187dc8c1983ffb4eff81fcd7afa
Trevor B <33526666+tbrixey@users.noreply.github.com>
2020-12-17 16:32:30 -0600

    Saving is now working

--- app/components/EditVideo/index.tsx
+++ app/components/EditVideo/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import styles from './EditVideo.css';
 import EditNote from '../EditNote';
+import { saveNotes, readNotes } from '../../utils/notes';
 
 type Props = {
   file: string;
@@ -24,13 +25,29 @@ export default function EditVideo({ file, onClose }: Props) {
     if (videoRef && videoRef.current) {
       setPaused(videoRef.current ? videoRef.current.paused : true);
     }
+    setNotes(readNotes(file));
   }, []);
+
+  const saveNotesToFS = () => {
+    saveNotes(notes, file);
+  };
+
+  const closeVideo = () => {
+    onClose();
+  };
 
   return (
     <div className={styles.root}>
       <div>
-        <button type="button" onClick={onClose}>
+        <button
+          type="button"
+          onClick={saveNotesToFS}
+          style={{ marginRight: 8 }}
+        >
           Save
+        </button>
+        <button type="button" onClick={closeVideo}>
+          Close
         </button>
       </div>
       <div className={styles.videoContainer}>
