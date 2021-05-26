447a380c6eb8a5189aa223651f4f6e791cf5330b
trevorb <brixey.trevor@gmail.com>
2021-05-26 16:54:28 -0500

    Add crypto donation addresses

--- src/components/MainMenu/index.tsx
+++ src/components/MainMenu/index.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import MenuIcon from "@material-ui/icons/Menu";
-import { IconButton, Menu, MenuItem } from "@material-ui/core";
+import {
+  Dialog,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  IconButton,
+  Menu,
+  MenuItem,
+} from "@material-ui/core";
+import BitcoinIcon from "../../bitcoin-btc-logo.svg";
+import { styled } from "@material-ui/core/styles";
+
+const MyDialogText = styled(DialogContentText)({
+  wordBreak: "break-all",
+});
 
 type Props = {
   onChangeFolder?: () => void;
 };
 export function MainMenu({ onChangeFolder }: Props) {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [walletDialog, setWalletDialog] = React.useState(false);
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
@@ -39,7 +54,36 @@ export function MainMenu({ onChangeFolder }: Props) {
       >
         <MenuItem onClick={handleChangeFolder}>Change Folder</MenuItem>
         <MenuItem onClick={handleClose}>About</MenuItem>
+        <MenuItem
+          onClick={() => {
+            setWalletDialog(true);
+            setAnchorEl(null);
+          }}
+        >
+          Donate <img src={BitcoinIcon} style={{ width: 24, marginLeft: 8 }} />
+        </MenuItem>
       </Menu>
+      <Dialog open={walletDialog} onClose={() => setWalletDialog(false)}>
+        <DialogTitle>Wallet Addresses</DialogTitle>
+        <DialogContent>
+          <MyDialogText>
+            <b>Bitcoin:</b> bc1qwgxtcwtcm2h3w0n4czng7sezlkhlvt3k97dujp
+          </MyDialogText>
+          <MyDialogText>
+            <b>Ethereum:</b> 0x4d58C7Afe1e9532CABFd3844Af8b73F932f785BE
+          </MyDialogText>
+          <MyDialogText>
+            <b>Cardano:</b>{" "}
+            addr1q9eyt59jc3ljkmc20xp6husynec24gv8py8a07v2x4ke6fmjghgt93rl9dhs57vr40eqf8ns42scwzg06luc5dtdn5nsf44a74
+          </MyDialogText>
+          <MyDialogText>
+            <b>Litecoin:</b> La6vGXnBnCi7wxi1rG6Q7TQrt9x8k7xP5H
+          </MyDialogText>
+          <MyDialogText>
+            <b>Cosmos:</b> cosmos1vaqv3v7j9j0kqc3dw460x3e7ayl5r7srqyv58a
+          </MyDialogText>
+        </DialogContent>
+      </Dialog>
     </>
   );
 }
